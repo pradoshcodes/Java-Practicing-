95ac8f0c67953909da63f295105afa268362a51a
pradoshcodes <pradoshmohanty034@gmail.com>
2024-04-16 18:27:04 +0530

    even or odd check

---
+++ EvenOdd.java
@@ -0,0 +1,10 @@
+public class EvenOdd {
+    public static void main(String[] args) {
+        int num = 10;
+        if(num % 2 ==0){
+            System.out.println(null + "The number is even");
+        }else{
+            System.out.print("The number is odd");
+        }
+    }
+}
