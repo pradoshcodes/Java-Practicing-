c351dfa54db8278e6afb71c43dbef43175144494
pradoshcodes <pradoshmohanty034@gmail.com>
2024-04-20 22:04:15 +0530

    Recursion program

--- Recursion.java
+++ Recursion.java
@@ -1,25 +1,36 @@
-public class Recursion {
-   
-    // Removed unnecessary instance variable 'fact'
+// public class Recursion {
+//    static int fact=1;
+//   public static void main(String[] args) {
+//    int num=5;
+//    Recursion  r = new Recursion();
+//    r.call(num);
+//    System.out.println("factorial of "+num+" is :"+fact);
+//   }
+//   void call(int num){
+//    if (num >=1) {
+//       fact=fact*num;
+//       call(num-1);
+//    }
+//   }
+//  }
  
-    // Modified the method to return an int (factorial value)
-    int call(int num) {
-      // Initialize fact to 1 for each call
-      int fact = 1;
-      if(num > 1){
-         // Calculate factorial recursively
-         fact = fact * call(num - 1);
-         System.out.print("*" + num);
-      }
-      return fact;
-    }
-    
-    public static void main(String[] args) {
-       int num = 5;
-       int res;
-       Recursion r = new Recursion(); // Changed to Recursion class
-       res = r.call(num); // Changed to call method from Recursion class
-       System.out.println(res); // Print the result
-    }
+
+import java.util.*;
+public class Recursion {
+   static int fact=1;
+  public static void main(String[] args) {
+   Scanner sc = new Scanner(System.in);
+   System.out.println("Enter a number:");
+   int num = sc.nextInt();
+   Recursion  r = new Recursion();
+   r.call(num);
+   System.out.println("factorial of "+num+" is :"+fact);
+  }
+  void call(int num){
+   if (num >=1) {
+      fact=fact*num;
+      call(num-1);
+   }
+  }
  }
  
