1d69bdbe7583ffc7a8b5fe140fcde9e540c5b75c
pradoshcodes <pradoshmohanty034@gmail.com>
2024-06-08 07:18:20 +0530

    string method

---
+++ String class/Stringmethod2.java
@@ -0,0 +1,29 @@
+// public class Stringmethod2 {
+//     public static void main(String[] args) {
+//         String s1="deepak";
+//         String s2="amit";
+//         String s3="deepak";
+//         System.out.println(s1.equals(s2));//false
+//         System.out.println(s1.equals(s3));//true
+//     }
+//     }
+
+
+
+// public class Stringmethod2 {
+//     public static void main(String[] args) {
+//         String s1="deepak";
+//         String s2="Deepak";
+//         System.out.println(s1.equalsIgnoreCase(s2));//true
+//     }
+//     }
+
+
+    public class Stringmethod2 {
+        public static void main(String[] args) {
+            String s1="a";
+            String s2="A";
+            System.out.println(s1.compareTo(s2));//false
+            
+        }
+        }
