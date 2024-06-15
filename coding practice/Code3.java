a3963ff331e8b6cca732813a17a22aa419ace60a
pradoshcodes <pradoshmohanty034@gmail.com>
2024-06-15 19:43:50 +0530

    check string is palindrome or not

---
+++ coding practice/Code3.java
@@ -0,0 +1,20 @@
+//String is palindrome or not
+public class Code3 {
+    public static void main(String[] args) {
+        String str="pradosh mohanty";
+        String revStr="";
+        for(int i=str.length()-1;i>=0;i--){
+            revStr=revStr+str.charAt(i);//The Java String class charAt() method returns a char value at the given index number.
+        }
+        System.out.println(revStr);
+        
+        if (revStr.equals(str)) {
+            System.out.println("String is a palindrome");
+        }else{
+            System.out.println("String is not a palindrome");
+        }
+        
+    }
+}
+
+
