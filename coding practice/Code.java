70d7cab6fab514d7309c841b2f65aad2a0ef91fe
pradoshcodes <pradoshmohanty034@gmail.com>
2024-06-24 09:31:35 +0530

    reverse number in 3 way

--- coding practice/Code.java
+++ coding practice/Code.java
@@ -1,1 +1,28 @@
-//remove duplicates from string in java  EX:sandeep    to     sandep
+//reverse a number 
+
+import java.util.Scanner;
+public class Code {
+
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter a number: ");
+        int num=sc.nextInt();
+        // int rev=0;
+        // while(num!=0){
+        //     rev=rev*10+num%10;
+        //     num=num/10;
+        // }
+
+        //Using StringBuffer
+        // StringBuffer bf=new StringBuffer(String.valueOf(num));
+        // StringBuffer rev=bf.reverse();
+        // System.out.println("Reverse of the number is: "+rev);
+
+
+        //Using StringBuilder
+         StringBuilder sb=new StringBuilder();
+         sb.append(num);
+         StringBuilder rev=sb.reverse();
+         System.out.println("Reverse of the number is: "+rev);
+    }
+}
