c939e4c78e0c54418cfb38df3d1bd85b892b456d
pradoshcodes <pradoshmohanty034@gmail.com>
2024-06-25 07:52:50 +0530

    Hackerank day 7

---
+++ Exception Handling/Exception2.java
@@ -0,0 +1,23 @@
+public class Exception2 {
+    public static void main(String[] args) {
+        // try {
+        //     int a=100,b=2,c;
+        //     c=a/b;
+        //     System.out.println(c);
+        // }catch(ArithmeticException e){
+        //     System.out.println("Error"+e);//if the  statement in try block is  execute it then goes to finally 
+        // }
+        // finally{
+        //     System.out.println("Finally block executed");
+        // }
+//         try {
+//                 int a=100,b=0,c;
+//                 c=a/b;
+//                 System.out.println(c);
+//         }
+//         finally{
+//             System.out.println("i am in finally block");//if you are using catch then it print only finally block and then abnormally terminate.
+//         }
+//         System.out.println("hello");
+//     }
+// }
