cc0cf0c30efc9c89ec1adafeee8f52d4b3e5ae9a
pradoshcodes <pradoshmohanty034@gmail.com>
2024-06-26 08:09:45 +0530

    Day 5

---
+++ coding practice/Code5.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+class Code5{
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter a number:");
+        int a=sc.nextInt();
+        int rev=0;
+
+        while (a!=0) {
+            rev=rev*10+ a%10;
+            a=a/10;
+
+        }
+        if (a==rev) {
+            System.out.println(rev+"number is pallindrome.");
+        }else{
+            System.out.println(rev+"number is not pallindrome.");
+        }
+    }
+}
