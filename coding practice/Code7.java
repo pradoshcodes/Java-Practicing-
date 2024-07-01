fef7ac3fed5afe3d8481ec2e6e3152cfb9654a81
pradoshcodes <pradoshmohanty034@gmail.com>
2024-07-01 09:48:06 +0530

    Learning day7

---
+++ coding practice/Code7.java
@@ -0,0 +1,17 @@
+//Fibonnaci series
+public class Code7 {
+    public static void main(String[] args) {
+        int n1=0;
+        int n2=1;
+        int sum;
+        System.out.print(n1+" "+n2);
+        for(int i=2;i<10;i++){
+            
+            sum=n1+n2;
+            System.out.print(" "+sum+" ");
+            n1=n2;
+            n2=sum;
+        }
+       
+    }
+}
