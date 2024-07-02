000cfa4d456f54f774f7c356f1c05657f7a7a763
pradoshcodes <pradoshmohanty034@gmail.com>
2024-07-02 07:36:55 +0530

    learning day8

---
+++ coding practice/Code8.java
@@ -0,0 +1,14 @@
+import java.util.Random;
+
+public class Code8 {
+    public static void main(String[] args) {
+        Random rd=new Random();
+        // int a=rd.nextInt(100);
+        // System.out.println(a);
+
+        // double rd_dbl=rd.nextDouble();
+        // System.out.println(rd_dbl);
+
+        System.out.println(Math.random());
+    }
+}
